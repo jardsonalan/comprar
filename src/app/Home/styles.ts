69064190a0b4a12b376c801532647a7277a7fc0e
Jardson Alan <jardsonalan132@gmail.com>
2026-02-16 15:00:43 -0300

    feat: Adiciona inicialização do projeto e estrutura de pastas

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f4f4f4'
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 700,
+    color: '#333'
+  }
+})
