2d8fd1eb877ba44c639e749a90bd0fd0a8ba99d1
Jardson Alan <jardsonalan132@gmail.com>
2026-02-16 15:58:46 -0300

    feat: Adiciona componente de Button utilizando propriedades

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -3,9 +3,10 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f4f4f4'
+    backgroundColor: '#f4f4f4',
+    paddingTop: 62,
+    padding: 24,
   },
   logo: {
     height: 34,
