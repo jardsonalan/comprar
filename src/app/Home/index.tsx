d5509dd2cb0cb23b65646f91d58421ff53cb3f16
Jardson Alan <jardsonalan132@gmail.com>
2026-02-16 20:08:57 -0300

    feat: Adiciona componentes de Input, Filter e Item com estilos e campo para limpar lista

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,52 @@
-import { View, Image } from 'react-native'
+import { View, Image, TouchableOpacity, Text, ScrollView } from 'react-native'
 import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+import { Filter } from '@/components/Filter'
+import { FilterStatus } from '@/types/FilterStatus'
 import { styles } from './styles'
+import { Item } from '@/components/Item'
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo}/>
-      <Button title='Adicionar' />
+      
+      <View style={styles.form}>
+        <Input placeholder='O que você precisa comprar?' />
+        <Button title='Adicionar' />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map(status => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView>
+          {
+            Array.from({ length: 100 }).map((value, index) =>
+              <Item
+                key={index}
+                data={{ status: FilterStatus.DONE, description: 'Leite' }}
+                onStatus={() => console.log('Status alterado')}
+                onRemove={() => console.log('Remover')}
+              />
+            )
+          }
+        </ScrollView>
+
+        <Item
+          data={{ status: FilterStatus.DONE, description: 'Leite' }}
+          onRemove={() => console.log('Remover')}
+          onStatus={() => console.log('Status alterado')} />
+      </View>
     </View>
   )
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -6,10 +6,41 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#f4f4f4',
     paddingTop: 62,
-    padding: 24,
   },
   logo: {
     height: 34,
     width: 134,
+  },
+  form: {
+    width: '100%',
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    paddingTop: 32,
+    marginTop: 24,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e4e6ec',
+    paddingBottom: 12,
+  },
+  clearButton: {
+    marginLeft: 'auto',
+  },
+  clearText: {
+    fontSize: 12,
+    fontWeight: 600,
+    color: '#828282',
   }
 })
